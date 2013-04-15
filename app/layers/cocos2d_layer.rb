0e02c83cbf4172cc0f419f53ec2a326733531059
Juan Jose Karam <juanjokaram@gmail.com>
2013-04-14 21:43:27 -0500

    Adds:   => Director return after configuration

--- app/layers/cocos2d_layer.rb
+++ app/layers/cocos2d_layer.rb
@@ -3,13 +3,12 @@ class Cocos2DLayer < Joybox::Core::Layer
   enable_scene
 
   def on_enter
-
+    
     title = Label.new text: 'Hello World',
                       font_size: 64,
                       position: [Screen.half_width, Screen.half_height]
 
     self << title
-
   end
 
 end
