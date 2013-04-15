3f9755482895ad7a9014ea18f469cab8a7cf2165
Juan Jose Karam <juanjokaram@gmail.com>
2013-04-14 22:05:25 -0500

    Adds:   => Sugar sintaxis to Menus

--- lib/joybox/cocos2d/ui/menu.rb
+++ lib/joybox/cocos2d/ui/menu.rb
@@ -4,6 +4,11 @@ module Joybox
     class Menu < CCMenu
 
       alias_method :align_items_vertically, :alignItemsVertically
+      alias_method :align_items_vertically_width_padding, :alignItemsVerticallyWithPadding
+      alias_method :align_items_horizontally, :alignItemsHorizontally
+      alias_method :align_items_horizontally_with_padding, :alignItemsHorizontallyWithPadding
+      alias_method :align_items_in_columns, :alignItemsInColumns
+      alias_method :align_items_in_rows, :alignItemsInRows
 
       def self.new(options = {})
 
