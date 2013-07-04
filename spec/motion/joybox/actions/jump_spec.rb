c66ea97c6af2a4818f5a28e1aa6a449c802a7278
CurveBeryl <public.juan.karam@me.com>
2013-07-03 22:41:20 -0500

    Fix build server

--- spec/motion/joybox/actions/jump_spec.rb
+++ spec/motion/joybox/actions/jump_spec.rb
@@ -7,14 +7,14 @@ describe Joybox::Actions::Jump do
 
     wait 0.2 do
       @sprite.position.should.not == CGPointMake(5, 5)
-      @sprite.position.x.should.not.be.close 20, 0.1
-      @sprite.position.y.should.not.be.close 20, 0.1
+      @sprite.position.x.should.not.be.close 20, 2
+      @sprite.position.y.should.not.be.close 20, 2
     end
 
     wait 0.4 do
       @sprite.position.should.not == CGPointMake(5, 5)
-      @sprite.position.x.should.be.close 20, 0.1
-      @sprite.position.y.should.be.close 20, 0.1
+      @sprite.position.x.should.be.close 20, 2
+      @sprite.position.y.should.be.close 20, 2
     end
   end
 
@@ -24,14 +24,14 @@ describe Joybox::Actions::Jump do
 
     wait 0.2 do
       @sprite.position.should.not == CGPointMake(5, 5)
-      @sprite.position.x.should.not.be.close 10, 0.1
-      @sprite.position.y.should.not.be.close 10, 0.1
+      @sprite.position.x.should.not.be.close 10, 2
+      @sprite.position.y.should.not.be.close 10, 2
     end
 
     wait 0.4 do
       @sprite.position.should.not == CGPointMake(5, 5)
-      @sprite.position.x.should.be.close 10, 0.1
-      @sprite.position.y.should.be.close 10, 0.1
+      @sprite.position.x.should.be.close 10, 2
+      @sprite.position.y.should.be.close 10, 2
     end
   end
 end
