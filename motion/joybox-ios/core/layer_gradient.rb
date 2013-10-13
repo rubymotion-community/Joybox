f1731c07d42441a27582c980c21727cb0dfbdb23
meganemura <mura2megane@gmail.com>
2013-10-14 01:00:18 +0900

    Add `Joybox::Core::LayerGradient`

---
+++ motion/joybox-ios/core/layer_gradient.rb
@@ -0,0 +1,9 @@
+module Joybox
+  module Core
+
+    class LayerGradient < CCLayerGradient
+      include TouchEvent
+    end
+
+  end
+end
---
+++ motion/joybox-osx/core/layer_gradient.rb
@@ -0,0 +1,13 @@
+module Joybox
+  module Core
+
+    class LayerGradient < CCLayerGradient
+
+      include MouseEvent
+      include KeyboardEvent
+      include GestureEvent
+
+    end
+
+  end
+end
